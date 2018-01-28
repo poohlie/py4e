eb462692c170451e93f53516df4e197bbf4293e3
poohlie <kid.sampson@gmail.com>
2018-01-28 21:18:01 +0800

    assignment94

---
+++ assignment94.py
@@ -0,0 +1,18 @@
+filename = input("Enter file:")
+handle = open(filename)
+names = dict()
+
+for line in handle:
+    pieces = line.split()
+    if len(pieces)!= 0 and pieces[0] == 'From':
+        names[pieces[1]] = names.get(pieces[1],0) + 1
+
+#to get the name with the maximum count
+maxname = None
+maxcount = 0
+for name in names:
+    if maxname == None or names[name]>maxcount:
+        maxname = name
+        maxcount = names[name]
+
+print(maxname, maxcount)
